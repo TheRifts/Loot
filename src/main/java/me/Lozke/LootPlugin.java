8b39b4371dc1a855e54723828130dbc245a42ed9
Lozke <joshuadavis1403@gmail.com>
2020-07-15 23:24:37 -0500

    Setup Repo

---
+++ src/main/java/me/Lozke/LootPlugin.java
@@ -0,0 +1,17 @@
+package me.Lozke;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LootPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onLoad() {
+
+    }
+
+}
