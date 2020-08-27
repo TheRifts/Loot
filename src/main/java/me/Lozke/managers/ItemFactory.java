d08872a9009fcb4348832a6276deda420818935e
Lozke <17802931+Lozke@users.noreply.github.com>
2020-08-26 21:57:07 -0500

    Massively optimized + Removed defunct dmg/hp handling

--- src/main/java/me/Lozke/managers/ItemFactory.java
+++ src/main/java/me/Lozke/managers/ItemFactory.java
@@ -13,17 +13,21 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemFactory {
 
+    private static ItemStack createItemStackFromTier(Tier tier, String gearType) {
+        return new ItemStack(Material.valueOf(tier.getArmorMaterial() + "_" + gearType));
+    }
+
     public static ItemStack newHelmet(Tier tier, Rarity rarity) {
-        return createItem(tier, rarity, tier.getArmorMaterial(), "_HELMET");
+        return createItem(tier, rarity, createItemStackFromTier(tier, "HELMET"), ItemType.ARMOR);
     }
     public static ItemStack newChestplate(Tier tier, Rarity rarity) {
-        return createItem(tier, rarity, tier.getArmorMaterial(), "_CHESTPLATE");
+        return createItem(tier, rarity, createItemStackFromTier(tier, "CHESTPLATE"), ItemType.ARMOR);
     }
     public static ItemStack newLeggings(Tier tier, Rarity rarity) {
-        return createItem(tier, rarity, tier.getArmorMaterial(), "_LEGGINGS");
+        return createItem(tier, rarity, createItemStackFromTier(tier, "LEGGINGS"), ItemType.ARMOR);
     }
     public static ItemStack newBoots(Tier tier, Rarity rarity) {
-        return createItem(tier, rarity, tier.getArmorMaterial(), "_BOOTS");
+        return createItem(tier, rarity, createItemStackFromTier(tier, "BOOTS"), ItemType.ARMOR);
     }
 
     //lol this would be the perfect place to return a Set<ItemStack>... just saying...
@@ -31,8 +35,8 @@ public class ItemFactory {
         return new ItemStack[]{newBoots(tier, rarity), newLeggings(tier, rarity), newChestplate(tier, rarity), newHelmet(tier, rarity)};
     }
 
-    public static ItemStack newWeapon(Tier tier, Rarity rarity, String type) {
-        return createItem(tier, rarity, tier.getWeaponMaterial(), "_" + type.toUpperCase());
+    public static ItemStack newWeapon(Tier tier, Rarity rarity, WeaponType weaponType) {
+        return createItem(tier, rarity, weaponType.getItem(tier), ItemType.WEAPON);
     }
 
     public static ItemStack newScrap(Tier tier) {
@@ -100,53 +104,24 @@ public class ItemFactory {
         return scroll.getItem();
     }
 
-    private static ItemStack createItem(Tier tier, Rarity rarity, String material, String itemType) {
-        ItemStack item = null;
-        ItemWrapper itemWrapper = null;
+    private static ItemStack createItem(Tier tier, Rarity rarity, ItemStack stack, ItemType itemType) {
+        ItemWrapper itemWrapper = new ItemWrapper(stack);
         switch (itemType) {
-            case "_HELMET":
-            case "_CHESTPLATE":
-            case "_LEGGINGS":
-            case "_BOOTS":
-                item = new ItemStack(Material.valueOf(material + itemType));
-                itemWrapper = new ItemWrapper(item);
-
+            case ARMOR:
                 if (NumGenerator.roll(2) == 1) {
                     itemWrapper.addKey(ARNamespacedKey.HP_REGEN, 50);
                 }
                 else {
                     itemWrapper.addKey(ARNamespacedKey.ENERGY_REGEN, 3);
                 }
-
-                int val = AgorianRifts.getGearData().getInt(tier.name() + "." + itemType.substring(1) + ".HI");
-                double rarityMultiplier = AgorianRifts.getGearData().getInt("MULTIPLIER.ARMOR." + rarity.name()),
-                        tierMultiplier = AgorianRifts.getGearData().getInt("MULTIPLIER.ARMOR." + tier.name());
-                int loRange = (int) Math.ceil(Math.ceil(val * rarityMultiplier) - Math.ceil(val * tierMultiplier)), hiRange = (int) Math.ceil(val * rarityMultiplier);
-
-                itemWrapper.addKey(ARNamespacedKey.HEALTH_POINTS, NumGenerator.rollInclusive(loRange, hiRange));
+                //Hardcoding to 0 hp till fallen blesses the code with a better algorithm
+                itemWrapper.addKey(ARNamespacedKey.HEALTH_POINTS, 0);
                 break;
-            case "_SWORD":
-            case "_AXE":
-            case "_SHOVEL":
-            case "_HOE":
-                item = new ItemStack(Material.valueOf(material + itemType));
-                itemWrapper = new ItemWrapper(item);
-
-                String valString = tier.name() + "." + itemType.substring(1);
-                int lo = AgorianRifts.getGearData().getInt(valString + ".LO"), hi = AgorianRifts.getGearData().getInt(valString + ".HI");
-                int mid = (lo+hi)/2;
-                double tierMulti = AgorianRifts.getGearData().getDouble("MULTIPLIER.WEAPON." + tier.name()), rarityMulti = AgorianRifts.getGearData().getDouble("MULTIPLIER.WEAPON." + rarity.name());
-
-                lo = (int) ((Math.ceil(Math.ceil(NumGenerator.rollInclusive(lo, mid) * rarityMulti) * tierMulti)) - Math.ceil(hi * 0.5));
-                hi = (int) (Math.ceil(Math.ceil(NumGenerator.rollInclusive(mid, hi) * rarityMulti) * tierMulti));
-
+            case WEAPON:
                 //Hardcoding to 1k dmg till fallen blesses the code with a better algorithm
                 itemWrapper.addKey(ARNamespacedKey.DMG_LO, 1000);
                 itemWrapper.addKey(ARNamespacedKey.DMG_HI, 1000);
                 break;
-        }
-        if (item == null) {
-            return null;
         }
 
         itemWrapper.addKey(ARNamespacedKey.REAL_ITEM);
@@ -159,10 +134,10 @@ public class ItemFactory {
 
         itemWrapper.randomizeAttributes();
 
-        item = itemWrapper.getItem();
-        ItemMeta itemMeta = item.getItemMeta();
+        stack = itemWrapper.getItem();
+        ItemMeta itemMeta = stack.getItemMeta();
         itemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-        item.setItemMeta(itemMeta);
-        return item;
+        stack.setItemMeta(itemMeta);
+        return stack;
     }
 }
