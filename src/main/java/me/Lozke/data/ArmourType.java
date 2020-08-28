566fbfedad860c1f6cb7cd80a88830785b69b610
Lozke <17802931+Lozke@users.noreply.github.com>
2020-08-28 18:38:43 -0500

    Removed tiered field

--- src/main/java/me/Lozke/data/ArmourType.java
+++ src/main/java/me/Lozke/data/ArmourType.java
@@ -6,19 +6,17 @@ import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
 public enum ArmourType {
-    HELMET("HELMET", EquipmentSlot.HEAD, true),
-    CHESTPLATE("CHESTPLATE", EquipmentSlot.CHEST, true),
-    LEGGINGS("LEGGINGS", EquipmentSlot.LEGS,true),
-    BOOTS("BOOTS", EquipmentSlot.FEET, true);
+    HELMET("HELMET", EquipmentSlot.HEAD),
+    CHESTPLATE("CHESTPLATE", EquipmentSlot.CHEST),
+    LEGGINGS("LEGGINGS", EquipmentSlot.LEGS),
+    BOOTS("BOOTS", EquipmentSlot.FEET);
 
     private String itemType;
     private EquipmentSlot equipmentSlot;
-    private boolean tiered;
 
-    ArmourType(String itemType, EquipmentSlot equipmentSlot, boolean tiered) {
+    ArmourType(String itemType, EquipmentSlot equipmentSlot) {
         this.itemType = itemType;
         this.equipmentSlot = equipmentSlot;
-        this.tiered = tiered;
     }
 
     public static ArmourType[] types = ArmourType.values();
@@ -34,9 +32,7 @@ public enum ArmourType {
         return getItem(Tier.T1);
     }
     public ItemStack getItem(Tier tier) {
-        Material material;
-        if (tiered) material = Material.getMaterial(tier.getArmorMaterial() + "_" + itemType);
-        else material = Material.getMaterial(itemType);
+        Material material = Material.getMaterial(tier.getArmorMaterial() + "_" + itemType);
         if (material == null) {
             material = Material.AIR;
             Logger.error("Invalid weapon material. Item Type: " + itemType + " Tier: " + tier);
