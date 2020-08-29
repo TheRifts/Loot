cddaf19bb4c36a29bc5c21e5bd3de2a2bc63f052
Lozke <17802931+Lozke@users.noreply.github.com>
2020-08-28 19:18:01 -0500

    Method to get ArmourType from EquipmentSlot

--- src/main/java/me/Lozke/data/ArmourType.java
+++ src/main/java/me/Lozke/data/ArmourType.java
@@ -31,6 +31,7 @@ public enum ArmourType {
     public ItemStack getItem() {
         return getItem(Tier.T1);
     }
+
     public ItemStack getItem(Tier tier) {
         Material material = Material.getMaterial(tier.getArmorMaterial() + "_" + itemType);
         if (material == null) {
@@ -38,5 +39,20 @@ public enum ArmourType {
             Logger.error("Invalid weapon material. Item Type: " + itemType + " Tier: " + tier);
         }
         return new ItemStack(material);
+    }
+
+    public static ArmourType fromEquipmentSlot(EquipmentSlot slot) {
+        switch (slot) {
+            case HEAD:
+                return ArmourType.HELMET;
+            case CHEST:
+                return ArmourType.CHESTPLATE;
+            case LEGS:
+                return ArmourType.LEGGINGS;
+            case FEET:
+                return ArmourType.BOOTS;
+            default:
+                return null;
+        }
     }
 }
