78a917c11ba9e96ae939ee89030b7f3bd4f73f4e
Lozke <17802931+Lozke@users.noreply.github.com>
2020-09-14 20:00:17 -0500

    Implement Stat Manager

--- src/main/java/me/Lozke/managers/EquipmentManager.java
+++ src/main/java/me/Lozke/managers/EquipmentManager.java
@@ -37,7 +37,7 @@ public class EquipmentManager {
             container.clearSlot(slot);
             return;
         }
-        Map<RiftsStat, Integer> itemStats = combineStatMaps(wrapper.getMinorStats(), wrapper.getMajorStats());
+        Map<RiftsStat, Integer> itemStats = StatManager.combineStatMaps(wrapper.getMinorStats(), wrapper.getMajorStats());
         container.setSlotHashCode(slot, stack.hashCode());
         container.setSlotStats(slot, itemStats);
     }
@@ -45,18 +45,4 @@ public class EquipmentManager {
     private boolean isSameHash(int hash1, int hash2) {
         return hash1 == hash2;
     }
-
-    @SafeVarargs
-    public static Map<RiftsStat, Integer> combineStatMaps(Map<RiftsStat, Integer>... statMaps) {
-        Map<RiftsStat, Integer> combinedMaps = new HashMap<>();
-        for (Map<RiftsStat, Integer> statMap : statMaps) {
-            for (Map.Entry<RiftsStat, Integer> stats : statMap.entrySet()) {
-                Integer oldValue = combinedMaps.getOrDefault(stats.getKey(), 0);
-                Integer combinedValue = oldValue + stats.getValue();
-                combinedMaps.put(stats.getKey(), combinedValue);
-            }
-        }
-        return combinedMaps;
-    }
-
 }
